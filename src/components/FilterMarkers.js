6ab241ce02d941ccda485111184a396bc73a8b0c
johnnyqbui <johnnyqbui7@gmail.com>
2017-08-30 14:29:12 -0500

    added filter for shelters added within the past 12 hours

--- src/components/FilterMarkers.js
+++ src/components/FilterMarkers.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import '../App.css';
 
 class FilterMarkers extends Component {
@@ -17,7 +18,9 @@ class FilterMarkers extends Component {
 		value === "Pets" 			 			   && ( filtered = origMarkers.filter(marker => (marker.pets.length > 0 && marker.pets.match(/yes/ig))))
 		value === "Shelters that need Volunteers"  && ( filtered = origMarkers.filter(marker => (marker.volunteerNeeds.length > 0)))
 		value === "Shelters that need Supplies"    && ( filtered = origMarkers.filter(marker => (marker.supplyNeeds.length > 0)))
-		value === "Added within the last 12 hours" && ( filtered = origMarkers.filter(marker => (console.log(marker.lastUpdated))))
+		value === "Added within the last 12 hours" && ( filtered = origMarkers.filter(marker => (
+			moment(marker.lastUpdated).add(12, 'hours').isAfter(moment().format('YYYY-MM-DD hh:mm A'))
+		)))
 
 		onClickFilter(filtered)
 	}
