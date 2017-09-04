ea94ca2b3de83a2e39166775e6ef08780a021890
Jason Nolasco <jason@orchidflux.com>
2017-09-04 05:02:42 -0500

    clean, faster map (mapbox vector) and experimental icons. made desktop inbox a LTR slidein

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,48 +1,37 @@
 import React, { Component } from 'react';
 import L from 'leaflet';
 import { Map, Marker, Popup, TileLayer, CircleMarker, ZoomControl } from 'react-leaflet';
-import blueMarker from '../images/marker-icon-blue.png';
-import greyMarker from '../images/marker-icon-grey.png';
-import shadowMarker from '../images/marker-shadow.png'
+import blueMarker from '../images/shelter-blue.png';
+import greyMarker from '../images/shelter-grey.png';
 
 const blueMarkerIcon = new L.icon({
 	iconUrl: blueMarker,
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	shadowUrl: shadowMarker,
-	shadowSize: [41, 41],
-    shadowAnchor: [12, 41],
-    popupAnchor: [0, -28]
+	iconSize: [30, 41],
+	iconAnchor: [15, 41],
+	popupAnchor: [0, -35]
 })
 
 const greyMarkerIcon = new L.icon({
 	iconUrl: greyMarker,
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	shadowUrl: shadowMarker,
-	shadowSize: [41, 41],
-    shadowAnchor: [12, 41],
-    popupAnchor: [0, -28]
+  iconSize: [18, 23],
+  iconAnchor: [9, 23],
+  popupAnchor: [0, -18]
 })
 
 class Lmap extends Component {
-
-	state = {
-		bounds: []
-	}
-
+		state = {
+			bounds: []
+		}
   	centerToMarker = (location) => {
   		this.setState({
   			bounds: location
   		})
   	}
-
   	resetBounds = () => {
   		this.setState({
   			bounds: []
   		})
   	}
-
 	render() {
 		const { bounds } = this.state;
 		const { markers, currentLocation, viewport, onToggleInfo, onClosePanel } = this.props;
@@ -57,7 +46,7 @@ class Lmap extends Component {
 			    trackResize={ true }
 			>
 		      <TileLayer
-		        url='https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'
+		        url='https://api.mapbox.com/styles/v1/jnolasco/cj75zemih4wc02srs353jlu05/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiam5vbGFzY28iLCJhIjoiY2oyYmVwNXViMDB1NjJxbXB2aHFlZnAzZyJ9.dY4H7Hzre0GJOeHBrkzIpg'
 		        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 		      />
 
