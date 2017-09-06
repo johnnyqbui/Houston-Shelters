965e38ca165abe4b3cf56fb12b4f1dbc4f6d9281
johnnyqbui <johnnyqbui7@gmail.com>
2017-09-06 13:44:40 -0500

    text number added

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -14,7 +14,7 @@ const TopNavBar = (props) => {
   return (
     <div className='top-navbar'>
         <h2>Irma Response</h2>
-        <p className="top-support">Call for help: <a href="tel:311">3-1-1</a></p>
+        <p className="top-support">Text For Help: <a href='tel:3462140739'>346-214-0739</a></p>
 
     </div>
   )
