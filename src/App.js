5152f05ebb3cda25ab1accf7fc09df7fc0772695
johnnyqbui <johnnyqbui7@gmail.com>
2017-09-06 12:36:44 -0500

    repositioned

--- src/App.js
+++ src/App.js
@@ -1,274 +1,165 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom'
 import './App.css';
-import TopNavBar from './components/TopNavBar';
+import TopNavBar from './components/TopNavBar'
 import Lmap from './components/Map';
 import GeoLocate from './components/GeoLocate';
-import AddShelter from './components/AddShelter';
+import AddShelter from './components/AddShelter'
 import FilterPanel from './components/FilterPanel';
-// import Search from './components/Search';
 import LoadingIcon from './components/LoadingIcon';
 import InfoBox from './components/InfoBox'
-
-import Credits from './pages/Credits';
 
 import * as SheltersApi from './utils/SheltersApi';
 
 class App extends Component {
-    state = {
-        isLoading: true,
-        allMarkers: [],
-        filteredMarkers: [],
-        tempFilteredMarkers: [],
-        viewport: {
-            center: [29.760427, -95.369803],
-            zoom: 9
+  state = {
+    isActive: false,
+    isLoading: true,
+    markers: [],
+    filteredMarkers: [],
+    viewport: {
+      center: [29.760427, -95.369803],
+      zoom: 9
+    },
+    currentLocation: [],
+    selectedFilter: 'Accepting People',
+    selectedMarker: {},
+    toggledInfo: false
+  }
+
+  async componentDidMount() {
+    const shelterData = await SheltersApi.getAll();
+    const allMarkerData = shelterData.shelters.map((shelters) => {
+      const { county,
+        shelter,
+        address,
+        city,
+        phone,
+        cleanPhone,
+        pets,
+        accepting,
+        latitude,
+        longitude,
+        last_updated,
+        supply_needs,
+        volunteer_needs,
+        notes } = shelters;
+      return {
+        county: county,
+        shelter: shelter,
+        address: address,
+        city: city,
+        phone: phone,
+        cleanPhone: cleanPhone,
+        accepting: accepting,
+        pets: pets,
+        location: {
+          lat: latitude ? parseFloat(latitude) : 0,
+          lng: longitude ? parseFloat(longitude) : 0
         },
-        currentLocation: [],
-        selectedFilter: 'Accepting People',
-        tempSelectedFilter: 'Accepting People',
-        selectedMarker: {},
-        toggledInfo: false,
-        toggledPanel: true,
-        toggledSearchBox: false,
-        query: ''
-    }
+        lastUpdated: last_updated,
+        supplyNeeds:  supply_needs,
+        volunteerNeeds: volunteer_needs,
+        notes: notes,
+        showInfo: false
+      }
+    });
 
-    async componentDidMount() {
-        // console.log('Component Mounted')
-        const shelterData = await SheltersApi.getAll();
-        const allMarkerData = shelterData.shelters.map((shelters) => {
-            const {
-                county,
-                shelter,
-                address,
-                city,
-                phone,
-                cleanPhone,
-                pets,
-                accepting,
-                latitude,
-                longitude,
-                updatedAt,
-                supply_needs,
-                volunteer_needs,
-                notes
-            } = shelters;
-            return {
-                county: county,
-                shelter: shelter,
-                address: address,
-                city: city,
-                phone: phone,
-                cleanPhone: cleanPhone,
-                accepting: accepting,
-                pets: pets,
-                location: {
-                    lat: latitude ? parseFloat(latitude) : 0,
-                    lng: longitude ? parseFloat(longitude) : 0
-                },
-                lastUpdated: updatedAt,
-                supplyNeeds: supply_needs,
-                volunteerNeeds: volunteer_needs,
-                notes: notes,
-                showInfo: false
-            }
-        });
-        // Set initial markers to be accepting
-        const initialMarkers = allMarkerData.filter(marker => (marker.accepting))
-        this.setState({
-            isLoading: false,
-            allMarkers: allMarkerData,
-            filteredMarkers: initialMarkers,
-            tempFilteredMarkers: initialMarkers
-        });
-    }
+    this.setState({
+      isLoading: false,
+      markers: allMarkerData,
+      // Accepting shelters set to default
+      filteredMarkers: allMarkerData.filter(marker => (marker.accepting)),
+    });
+  }
 
-    // // For debugging purposes
-    // componentWillMount() {
-    //     console.log('Component is about to mount')
-    // }
+  handleFilteredList = (selectedFilter, filteredMarkers) => {
+    this.setState({
+      filteredMarkers: filteredMarkers,
+      selectedFilter: selectedFilter
+    })
+  }
 
-    // componentWillReceiveProps(nextProps) {
-    //     console.log('Component is about to receive props', nextProps)
-    // }
+  handleLocate = (currentLocation) => {
+    this.setState({
+      viewport: {
+        center: currentLocation,
+        zoom: 13
+      },
+      currentLocation: currentLocation
+    })
+  }
 
-    // shouldComponentUpdate(nextProps, nextState) {
-    //     console.log('Should component update?')
-    //     console.log('Next Props', nextProps)
-    //     console.log('Next State', nextState)
-    //     return true
-    // }
+  handleClosePanel = () => {
+    this.setState({
+      isActive: false
+    })
+  }
 
-    // componentWillUpdate() {
-    //     console.log('Component is about to update')
-    // }
+  handleTogglePanel = () => {
+    this.setState({
+      isActive: !this.state.isActive
+    })
+  }
 
-    // componentDidUpdate() {
-    //     console.log('Component has updated')
-    // }
+  handleToggleInfo = (marker) => {
+    this.setState({
+      selectedMarker: marker,
+      toggledInfo: !this.state.toggledInfo
+    })
+  }
 
-    // componentWillUnmount() {
-    //     console.log('Component is about to unmount')
-    // }
+  render() {
+    const {
+      isActive,
+      isLoading,
+      markers,
+      filteredMarkers,
+      viewport,
+      currentLocation,
+      selectedMarker,
+      toggledInfo } = this.state;
+    return (
+      <div className="App">
 
+        <TopNavBar />
 
-    handleFilteredMarkers = (selectedFilter, filteredMarkers) => {
-        this.setState({
-            filteredMarkers: filteredMarkers,
-            tempFilteredMarkers: filteredMarkers,
-            selectedFilter: selectedFilter,
-            tempSelectedFilter: selectedFilter
-        })
-    }
+        { isLoading && (<LoadingIcon />)}
+        { !isLoading && (
+          <FilterPanel
+            isActive={ isActive }
+            origMarkers={ markers }
+            filterLength={ filteredMarkers.length }
+            toggledInfo={ toggledInfo }
+            onTogglePanel={ this.handleTogglePanel }
+            onClickFilter={ this.handleFilteredList }
+          />
+        )}
 
-    handleSelectedFilters = (selectedFilter) => {
-        this.setState({
-            selectedFilter: selectedFilter
-        })
-    }
+        <GeoLocate
+          currentLocation={ currentLocation }
+          onClickLocate={ this.handleLocate }
+        />
 
-    handleLocate = (currentLocation) => {
-        this.setState({
-            viewport: {
-                center: currentLocation,
-                zoom: 13
-            },
-            currentLocation: currentLocation
-        })
-    }
+        <AddShelter />
 
-    handleClosePanel = () => {
-        this.setState({
-            toggledPanel: false
-        })
-    }
+        <Lmap
+          currentLocation={ currentLocation }
+          markers={ filteredMarkers }
+          viewport={ viewport }
+          onToggleInfo={ this.handleToggleInfo }
+          onTogglePanel={ this.handleTogglePanel }
+          onClosePanel={ this.handleClosePanel }
+        />
 
-    handleTogglePanel = () => {
-        this.setState({
-            toggledPanel: !this.state.toggledPanel
-        })
-    }
-
-    handleOpenInfoBox = (marker) => {
-        this.setState({
-            selectedMarker: marker,
-            toggledInfo: true
-        })
-    }
-
-    handleCloseInfoBox = () => {
-        this.setState({
-            toggledInfo: false
-        })
-    }
-
-    handleOpenSearchBox = () => {
-        this.setState({
-            toggledSearchBox: true
-        })
-    }
-
-    handleCloseSearchBox = () => {
-        this.setState({
-            toggledSearchBox: false
-        })
-    }
-
-    handleInputSearch = (query, selectedFilter) => {
-        console.log(selectedFilter)
-        this.setState({
-            filteredMarkers: query,
-            selectedFilter: selectedFilter
-        })
-    }
-
-    handleClickSearch = (matched, query) => {
-        this.setState({
-            viewport: {
-                center: [matched.location.lat, matched.location.lng],
-                zoom: 14
-            },
-            selectedMarker: matched,
-            filteredMarkers: [matched],
-            toggledInfo: true,
-            query: query
-        })
-    }
-
-    render() {
-        const {
-            isLoading,
-            allMarkers,
-            filteredMarkers,
-            tempFilteredMarkers,
-            selectedFilter,
-            tempSelectedFilter,
-            viewport,
-            currentLocation,
-            selectedMarker,
-            toggledInfo,
-            toggledPanel,
-            toggledSearchBox,
-            query } = this.state;
-        return (
-            <div className="App">
-
-                    <TopNavBar />
-
-                    <Route exact path='/' render={() => (
-                        <div>
-
-                        { isLoading ? <LoadingIcon /> :
-                            <FilterPanel
-                                toggledPanel={ toggledPanel }
-                                allMarkers={ allMarkers }
-                                filterLength={ filteredMarkers.length }
-                                selectedFilter={ selectedFilter }
-                                toggledInfo={ toggledInfo }
-
-                                onTogglePanel={ this.handleTogglePanel }
-                                onClickFilter={ this.handleFilteredMarkers }
-                                onCloseSearchBox={ this.handleCloseSearchBox }
-                            />
-                        }
-
-                        <GeoLocate
-                            currentLocation={ currentLocation }
-                            onClickLocate={ this.handleLocate }
-                        />
-
-                        <AddShelter />
-
-                        <Lmap
-                            currentLocation={ currentLocation }
-                            filteredMarkers={ filteredMarkers }
-                            allMarkers={ allMarkers }
-                            viewport={ viewport }
-                            selectedMarker={ selectedMarker }
-
-                            onOpenInfoBox={ this.handleOpenInfoBox }
-                            onCloseInfoBox={ this.handleCloseInfoBox }
-
-                            onTogglePanel={ this.handleTogglePanel }
-                            onClosePanel={ this.handleClosePanel }
-
-                            onCloseSearchBox={ this.handleCloseSearchBox }
-                        />
-
-                        <InfoBox
-                            toggledInfo={ toggledInfo }
-                            selectedMarker={ selectedMarker }
-                            query={ query }
-                        />
-                    </div>
-                )} />
-
-
-
-            </div>
-        )
-    }
+        <InfoBox
+          className='info-bar'
+          toggledInfo={ toggledInfo }
+          selectedMarker={ selectedMarker }
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
