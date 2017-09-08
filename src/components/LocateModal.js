7d3fffa5de8e910c53511e3601669dda101637ae
johnnyqbui <johnnyqbui7@gmail.com>
2017-09-08 15:17:06 -0500

    json modified

---
+++ src/components/LocateModal.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const LocateModal = (props) => {
+    const { showModal, currentLocation, onClickLocate } = props;
+    const locate = () => {
+        const success = (position) => {
+            const currentLocation = [position.coords.latitude, position.coords.longitude]
+            onClickLocate(currentLocation)
+        }
+        const error = (err) => { console.log('Could not obtain location', err)}
+
+        // High accuracy on false to consume energy on mobile
+        const options = {maximumAge: 60000, timeout: 5000, enableHighAccuracy: false}
+
+        !navigator.geolocation ?
+        console.log('Browser does not support Geolocation') :
+        navigator.geolocation.getCurrentPosition(success, error, options)
+        // navigator.geolocation.watchPosition(success, error, options)
+    }
+
+    return (
+        <button
+            className='locate-modal'
+            onClick={() => {locate(currentLocation)}}>
+            Locate My Position
+        </button>
+    )
+}
+
+export default LocateModal
