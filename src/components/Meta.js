ffacd23a3cfbaa99613af6cddf5df9ab4cf95482
johnnyqbui <johnnyqbui7@gmail.com>
2017-09-09 13:29:26 -0500

    testing meta tags

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -14,32 +14,6 @@ const Meta = () => {
         rel='shortcut icon'
         href={config.meta.favicon}
         type='image/x-icon'/>
-      {Object.entries(config.openGraph).map((entry, index) => {
-        if (entry[1]) {
-          return (
-            <meta
-              property={`og:${entry[0]}`}
-              content={entry[1]}
-              key={`meta-og-${index}`}/>
-          );
-        } else {
-          return null;
-        }
-      })}
-
-      {Object.entries(config.twitter).map((entry, index) => {
-        if (entry[1]) {
-          return (
-            <meta
-              name={`twitter:${entry[0]}`}
-              content={entry[1]}
-              key={`meta-twitter-${index}`}/>
-          );
-        } else {
-          return null;
-        }
-      })}
-
     </Helmet>
   );
 }
