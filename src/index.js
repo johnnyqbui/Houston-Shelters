8e33667ff73a814449ccffafcae62925185c3712
johnnyqbui <johnnyqbui7@gmail.com>
2017-09-17 14:16:43 -0500

    add static.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -16,6 +16,6 @@ ReactGA.set({ page: window.location.pathname });
 ReactGA.pageview(window.location.pathname);
 
 ReactDOM.render(
-	<HashRouter><App /></HashRouter>,
+	<BrowserRouter><App /></BrowserRouter>,
 	document.getElementById('root'));
 registerServiceWorker();
